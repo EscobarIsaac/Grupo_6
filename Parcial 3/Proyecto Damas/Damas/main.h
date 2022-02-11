c3c9336b92f71a4804a86946cc5d71300526b506
JosueAFerrin <jaferrin@espe.edu.ec>
2022-02-11 03:09:29 -0500

    Proyecto Damas

---
+++ Parcial 3/Proyecto Damas/Damas/main.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+#include<iostream>
+#include <thread>
+#include "main.h"
+
+#include "Control.h"
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+void exit_program();
+void primerMenu();
+void placeholder();
+void marquesina(std::string text, size_t limit);
+void AjustarVentana();
